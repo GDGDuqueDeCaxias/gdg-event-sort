f5224f11ed50b50f0fb2148a9094252965f79c90
Igor Felipe <igor_play_22@hotmail.com>
2016-09-10 01:28:10 -0300

    Changed language to english

--- js/app.js
+++ js/app.js
@@ -1,32 +1,26 @@
 var app = angular.module('plunker', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.csv = '';
-  $scope.json = '';
-  $scope.sorteado = 'GDG Sistema de Sorteios';
-  $scope.convertCsv = function(){
-
-    console.log($scope.fileContent + '1');
+app.controller('MainCtrl', function ($scope) {
+  $scope.raffled = 'GDG Sort System';
+  $scope.convertCsv = function () {
     $scope.json = Papa.parse($scope.fileContent);
-    console.log($scope.json);
     var numId = Math.floor(Math.random() * $scope.json.data.length);
-    console.log(numId);
-    var sorteado = $scope.json.data[numId];
-    $scope.sorteado = sorteado;
+    var result = $scope.json.data[numId];
+    $scope.raffled = result;
   };
 });
 
-app.directive('fileReader', function() {
+app.directive('fileReader', function () {
   return {
     scope: {
       fileReader:"="
     },
     link: function(scope, element) {
-      $(element).on('change', function(changeEvent) {
+      $(element).on('change', function (changeEvent) {
         var files = changeEvent.target.files;
         if (files.length) {
           var r = new FileReader();
-          r.onload = function(e) {
+          r.onload = function (e) {
               var contents = e.target.result;
               scope.$apply(function () {
                 scope.fileReader = contents;
